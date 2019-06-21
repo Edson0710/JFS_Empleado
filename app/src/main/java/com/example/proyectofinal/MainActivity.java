beb376b72f6c543b423e7437cff0d3d32beef8bf
Edson0710 <edsonnavarro0710@gmail.com>
2019-06-21 10:46:15 -0500

    Login

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -5,11 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_registrar;
+    EditText correoET, passET;
+    String correo, pass;
     Button boton_iniciar;
 
     @Override
@@ -18,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv_registrar= (TextView) findViewById(R.id.tv_Registrar);
+        correoET = (EditText) findViewById(R.id.Usuario);
+
         tv_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,4 +52,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+   /* public void login() {
+            String url = "http://jfsproyecto.online/loginEmpleado.php?correo=" + correo +
+                "&pass=" + pass;
+        final Intent intent = new Intent(Login.this, Vista_principal.class);
+        JsonObjectRequest peticion = new JsonObjectRequest
+                (
+                        Request.Method.GET,
+                        url,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String valor = response.getString("Estado");
+
+                                    switch (valor) {
+                                        case "EXITOSO":
+                                            startActivity(intent);
+                                            break;
+                                        case "FALLIDO":
+                                            Toast.makeText(Login.this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+                                            break;
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        , new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this, "Error conexión", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue x = Volley.newRequestQueue(Login.this);
+        x.add(peticion);
+    }*/
 }
